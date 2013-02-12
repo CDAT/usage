4bfc48bc87ce0145e4f93733ad1ff088ca728fe0
Matthew Harris <mattbenharris@gmail.com>
2013-02-12 14:28:48 -0800

    MBH: adding in examples and start of stats graphs

---
+++ examples/check.php
@@ -0,0 +1,29 @@
+<?php
+   
+   $link = mysql_connect('localhost', $db_user, $db_pass);
+   if (!$link){
+    die('Could not connect: ' . mysql_error());
+  	echo 'Connection Failed';
+  }
+  //echo 'Connected Successfully <br/>';
+
+	$db_selected = mysql_select_db($db_name, $link);
+	if (!$db_selected) {
+		die ('Can\'t use ' . $db_name . ' : ' . mysql_error());
+	}
+	//echo 'Using ' . $db_name;
+	$getdata = mysql_query("select distinct machines.platform as name, count(*) as value from users, machines where users.machine = machines.id group by machines.platform;");
+
+	$myFile = "data.csv";
+	$fh = fopen($myFile, 'w') or die("can't open file");
+	$header = "name,value\n";
+	fwrite($fh, $header);
+	while($show = mysql_fetch_array($getdata)){
+		$stringData =  $show[name] . "," . $show[value] . "\n"; 
+		fwrite($fh, $stringData);
+	}	
+	
+	fclose($fh);
+	
+	mysql_close($link); 
+?>
