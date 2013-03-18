50d54c57fa12a4cc6999fdb78cdc284a7b89fa38
Daniel Fedor-Thurman <fedorthurman1@llnl.gov>
2013-03-18 09:08:06 -0700

    Fixed url patterns allowing for appending arbitrary strings to a URL and having it still match
    It turns out I don't need to write GET parameters into the regexes, Django handles it for me.

--- app/urls.py
+++ app/urls.py
@@ -16,29 +16,29 @@ urlpatterns = patterns('app.views',
 
     # /log/json/domain/
     # /log/json/domain/?days=5
-    url(r'^json/domain/.*','ajax_getDomainInfo'),
+    url(r'^json/domain/$','ajax_getDomainInfo'),
 
     # /log/json/platform/
     # /log/json/platform/?days=25
     # (?!regex) == negative lookahead. Only matches if "platform/" is NOT followed by "details"
-	url(r'^json/platform/(?!details).*', 'ajax_getPlatformInfo'),
+	url(r'^json/platform/$', 'ajax_getPlatformInfo'),
 
     # /log/json/platform/details/
     # /log/json/platform/details/?days=32
-	url(r'^json/platform/details/.*', 'ajax_getDetailedPlatformInfo'),
+	url(r'^json/platform/details/$', 'ajax_getDetailedPlatformInfo'),
 
     # /log/json/source/
     # /log/json/source/?days=22
     # (?!regex) == negative lookahead. Only matches if "source/" is NOT followed by "details"
-	url(r'^json/source/(?!details).*', 'ajax_getSourceInfo'),
+	url(r'^json/source/(?!details)$', 'ajax_getSourceInfo'),
 
     # /log/json/source/details/
     # /log/json/source/details/?days=15
-	url(r'^json/source/details/.*', 'ajax_getDetailedSourceInfo'),
+	url(r'^json/source/details/$', 'ajax_getDetailedSourceInfo'),
 
     # /log/json/country/
     # /log/json/country/?days=7
-    url(r'^json/country/.*','ajax_getCountryInfo'),
+    url(r'^json/country/$','ajax_getCountryInfo'),
 
     # /log/json/details/
     url(r'^json/details/$', 'ajax_getLogDetails'),
