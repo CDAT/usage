b86db3b881d5c720fedb9d47c64e096df6cf53d1
Charles Doutriaux <doutriaux1@llnl.gov>
2013-04-02 08:32:28 -0700

    Local tweaks. DO NOT PUSH

--- live/settings.py
+++ live/settings.py
@@ -12,9 +12,9 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'testuvcdat',                      # Or path to database file if using sqlite3.
-        'USER': 'uvcdatlogs',                      # Not used with sqlite3.
-        'PASSWORD': 'uvcdat',                  # Not used with sqlite3.
+        'NAME': 'uvcdatusage',                      # Or path to database file if using sqlite3.
+        'USER': 'uvcdatusage',                      # Not used with sqlite3.
+        'PASSWORD': 'bpapsawc1',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
