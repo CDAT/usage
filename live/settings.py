0655fabdcb7f1403785af3d8886d870eadb559b4
Daniel Fedor-Thurman <fedorthurman1@llnl.gov>
2013-05-24 13:14:13 -0700

    Use local_settings.py to override parts of settings.py
    Rather than parsing settings.local at the top of settings.py

--- live/settings.py
+++ live/settings.py
@@ -1,52 +1,20 @@
-# Local customization file loaded in , conatins password and all, not in repo
-import os,sys
-
-f=open(os.getcwdu()+"/settings.local")
-dbname = f.readline().strip()
-dbuser=f.readline().strip()
-dbpassword=f.readline().strip()
-dbhost=f.readline().strip()
-mediapth = f.readline().strip()
-f.close()
-
-# Django settings for live project.
-
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
-
-ADMINS = (
-    # ('leung25', 'leung25@llnl.gov'),
-    ('fedorthurman1', 'fedorthurman1@llnl.gov'),
-)
-
-MANAGERS = ADMINS
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',# Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': dbname,                      # Or path to database file if using sqlite3.
-        'USER': dbuser,                      # Not used with sqlite3.
-        'PASSWORD': dbpassword,              # Not used with sqlite3.
-        'HOST': dbhost,                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                          # Set to empty string for default. Not used with sqlite3.
-    }
-}
-
-# A list of strings representing the host/domain names that this Django site can serve.
-# This is a security measure to prevent an attacker from poisoning caches and password
-# reset emails with links to malicious hosts by submitting requests with a fake HTTP Host
-# header, which is possible even under many seemingly-safe webserver configurations.
-ALLOWED_HOSTS = ['uvcdat.llnl.gov', '127.0.0.1', 'localhost']
-
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Los_Angeles'
-
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+################################################################################
+#
+#                DEFAULT SETTINGS, DO NOT MAKE YOUR CHANGES HERE!
+#         YOU MUST HAVE A `local_settings.py` IN ORDER FOR THIS TO RUN
+# If you do not have a `local_settings.py, run the following command:
+#     cp local_settings.py.template local_settings.py
+#
+# Then,open local_settings.py in your favorite editor and follow the
+# instructions.
+# 
+# These settings are over-ridden by local_settings.py
+# That is where you should make all your changes.
+#
+# If you would like to change a setting that is not in local_settings.py, copy
+# it from this file and make ALL changes in local_settings.py
+#
+################################################################################
 
 SITE_ID = 1
 
@@ -60,25 +28,6 @@ USE_L10N = True
 
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
-
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '%s/app/media/' % mediapth
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = 'http://localhost:8080/media/'
-
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/var/www/emily.llnl.gov/'
-
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
-STATIC_URL = 'http://127.0.0.1/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
@@ -94,9 +43,6 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
-
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = '7u&amp;*d3di%e*7fuu$^-eeteu(mk_e+usb3$g7jv9a%&amp;!&amp;#eyt#-'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
@@ -124,7 +70,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '%s/app' % mediapth
+    # '/example/uvcdat/usage/templates'
 )
 
 INSTALLED_APPS = (
@@ -186,5 +132,9 @@ LOGGING = {
         },
     }
 }
-GEOLITECITY_RELATIVE_PATH = "GeoIP/GeoLiteCity.dat"
-GEOORGANIZATION_RELATIVE_PATH = "GeoIP/GeoOrganization.dat"
+
+# if it exists, override these settings with the ones in local_settings.py
+try:
+    from local_settings import *
+except ImportError:
+    pass
