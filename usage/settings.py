1d36c28a6b355feb8640dd689e83269dafe1d96f
Matthew Harris <mattbenharris@gmail.com>
2017-04-04 07:25:07 -0700

    MBH: cleanup

--- usage/settings.py
+++ usage/settings.py
@@ -110,6 +110,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     'twitter_bootstrap',
+    'django_extensions',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
